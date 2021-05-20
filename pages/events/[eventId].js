a379b27f350dc81ec341153cf13086393a620d65
Dmitriy <shalberkind@mail.ru>
2021-05-20 14:51:13 +0300

    Implement event detail component

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,8 +1,30 @@
+import { useRouter } from 'next/router';
+import { getEventById } from '../../dummy-data';
+import EventSummary from '../../components/event-detail/event-summary';
+import EventLogistics from '../../components/event-detail/event-logistics';
+import EventContent from '../../components/event-detail/event-content';
+
 function EventDetailPage() {
+  const router = useRouter();
+
+  const eventId = router.query.eventId;
+  const event = getEventById(eventId);
+
+  if (!event) {
+    return <p>No event found</p>;
+  }
+
   return (
-    <div>
-      <h1>Event detail page</h1>
-    </div>
+    <>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.imageAlt}
+      />
+      <EventContent>{event.description}</EventContent>
+    </>
   );
 }
 
