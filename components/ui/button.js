046747d1f049dcd842c5734789eef13ddc57b8b9
Dmitriy <shalberkind@mail.ru>
2021-05-21 13:07:12 +0300

    Unify button component

--- components/ui/button.js
+++ components/ui/button.js
@@ -1,11 +1,19 @@
 import Link from 'next/link';
 import styles from './button.module.css';
 
-function Button({ children, link }) {
+function Button({ children, link, onClick }) {
+  if (link) {
+    return (
+      <Link href={link}>
+        <a className={styles.btn}>{children}</a>
+      </Link>
+    );
+  }
+
   return (
-    <Link href={link}>
-      <a className={styles.btn}>{children}</a>
-    </Link>
+    <button className={styles.btn} onClick={onClick}>
+      {children}
+    </button>
   );
 }
 
