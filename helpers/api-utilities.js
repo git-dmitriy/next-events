0f880a525c4065ceb92cd51ddf75351617ec5484
Dmitriy <shalberkind@mail.ru>
2021-05-26 23:52:19 +0300

    Create api-utilities

---
+++ helpers/api-utilities.js
@@ -0,0 +1,22 @@
+export async function getAllEvents() {
+  const response = await fetch(
+    'https://next-events-5820c-default-rtdb.asia-southeast1.firebasedatabase.app/events.json'
+  );
+  const data = response.json();
+
+  const events = [];
+
+  for (key in data) {
+    events.push({
+      id: key,
+      ...data[key],
+    });
+  }
+  return events;
+}
+
+export async function getFeaturedEvents() {
+  const allEvents = await getAllEvents();
+
+  return allEvents.filter((event) => event.isFeatured);
+}
