cb813d32d6d1cd2033d9054361516c61f9572971
Dmitriy <shalberkind@mail.ru>
2021-05-27 13:54:14 +0300

    Loading data & paths for dynamic pages

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,16 +1,12 @@
-import { useRouter } from 'next/router';
-import { getEventById } from '../../dummy-data';
+import { getEventById, getAllEvents } from '../../helpers/api-utilities';
 import EventSummary from '../../components/event-detail/event-summary';
 import EventLogistics from '../../components/event-detail/event-logistics';
 import EventContent from '../../components/event-detail/event-content';
 import Button from '../../components/ui/button';
 import ErrorAlert from '../../components/ui/error-alert';
 
-function EventDetailPage() {
-  const router = useRouter();
-
-  const eventId = router.query.eventId;
-  const event = getEventById(eventId);
+function EventDetailPage(props) {
+  const event = props.selectedEvent;
 
   if (!event) {
     return (
@@ -39,4 +35,23 @@ function EventDetailPage() {
   );
 }
 
+export async function getStaticProps(context) {
+  const eventId = context.params.eventId;
+  const event = await getEventById(eventId);
+
+  return {
+    props: {
+      selectedEvent: event,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  const allEvents = await getAllEvents();
+  const paths = allEvents.map((event) => ({ params: { eventId: event.id } }));
+  return {
+    paths,
+    fallback: false,
+  };
+}
 export default EventDetailPage;
