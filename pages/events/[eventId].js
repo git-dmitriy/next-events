fb76280908f1351204ea7dabaaef08a22c9d3ecc
Dmitriy <shalberkind@mail.ru>
2021-05-28 22:23:07 +0300

    Configuring the head content

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,9 +1,9 @@
+import Head from 'next/head';
 import { getEventById, getFeaturedEvents } from '../../helpers/api-utilities';
 import EventSummary from '../../components/event-detail/event-summary';
 import EventLogistics from '../../components/event-detail/event-logistics';
 import EventContent from '../../components/event-detail/event-content';
 import Button from '../../components/ui/button';
-import ErrorAlert from '../../components/ui/error-alert';
 
 function EventDetailPage(props) {
   const event = props.selectedEvent;
@@ -11,6 +11,10 @@ function EventDetailPage(props) {
   if (!event) {
     return (
       <>
+        <Head>
+          <title>{event.title}</title>
+          <meta name='description' content={event.description} />
+        </Head>
         <div className='center'>
           <p>Loading...</p>
         </div>
