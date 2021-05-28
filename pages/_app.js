36f364c2aafb8540d60f7b264075e2f165fdb2e9
Dmitriy <shalberkind@mail.ru>
2021-05-28 23:44:34 +0300

    Add title and meta description

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,8 @@ function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Head>
+        <title>Next Events</title>
+        <meta name='description' content='NextJS Events application.' />
         <meta name='viewport' content='initial-scale=1.0, width=device-width' />
       </Head>
       <Component {...pageProps} />
