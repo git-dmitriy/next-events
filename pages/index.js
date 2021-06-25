0e925424b1f6d6a6a8057d59317b77ed620f88c7
Dmitriy <shalberkind@mail.ru>
2021-06-25 23:56:01 +0300

    Create a newsletter route

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head';
 
 import { getFeaturedEvents } from '../helpers/api-utilities';
 import EventList from '../components/events/event-list';
+import NewsletterRegistration from '../components/input/newsletter-registration';
 
 function HomePage(props) {
   const { events } = props;
@@ -10,10 +11,11 @@ function HomePage(props) {
       <Head>
         <title>Events app</title>
         <meta
-          name='description'
-          content='This application was created in the next course.'
+          name="description"
+          content="This application was created in the next course."
         />
       </Head>
+      <NewsletterRegistration />
       <EventList items={events} />
     </div>
   );
