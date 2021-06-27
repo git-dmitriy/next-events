4ae10b2433479cd4644e5f860632855e7796f03a
Dmitriy <shalberkind@mail.ru>
2021-06-27 15:22:01 +0300

    Use notification context

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -1,10 +1,24 @@
+import { useContext } from 'react';
 import MainHeader from './main-header';
+import Notification from '../ui/notification';
+import NotificationContext from '../../store/notification-context';
 
 function layout({ children }) {
+  const notificationCtx = useContext(NotificationContext);
+
+  const activeNotification = notificationCtx.notification;
+
   return (
     <>
       <MainHeader />
       <main>{children}</main>
+      {activeNotification && (
+        <Notification
+          title={activeNotification.title}
+          message={activeNotification.message}
+          status={activeNotification.status}
+        />
+      )}
     </>
   );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,23 @@
 import Layout from '../components/layout/layout';
 import '../styles/globals.css';
 import Head from 'next/head';
+import { NotificationContextProvider } from '../store/notification-context';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Head>
-        <title>Next Events</title>
-        <meta name='description' content='NextJS Events application.' />
-        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
-      </Head>
-      <Component {...pageProps} />
-    </Layout>
+    <NotificationContextProvider>
+      <Layout>
+        <Head>
+          <title>Next Events</title>
+          <meta name='description' content='NextJS Events application.' />
+          <meta
+            name='viewport'
+            content='initial-scale=1.0, width=device-width'
+          />
+        </Head>
+        <Component {...pageProps} />
+      </Layout>
+    </NotificationContextProvider>
   );
 }
 
